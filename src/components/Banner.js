e2310c1bf16ac9951d23245439957c24c4c32817
Jose Manuel <jmdcarq89@gmail.com>
2021-04-05 20:47:41 +0200

    media object

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,18 +1,30 @@
 import styled from "styled-components";
 
+const screen = {
+  phone: (...args) => {
+    const styles = args;
+
+    return `@media (min-width:900px){
+            ${styles};
+        }`;
+  },
+};
+
 export const Banner = styled.div`
   background: red;
   h1 {
     color: white;
     text-transform: uppercase;
   }
-  @media (min-width: 576px) {
+  ${screen.phone`background:blue;h1{
+      color:yellow
+  }`}/* @media (min-width: 576px) {
     background: blue;
     h1 {
       color: yellow;
       text-transform: uppercase;
     }
-  }
+  } */
 `;
 
 export const SecondBanner = styled.div`
