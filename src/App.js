8a1fb969f775eb2be909ed5da13437d3e12a2e7a
Jose Manuel <jmdcarq89@gmail.com>
2021-04-05 20:08:20 +0200

    Button

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import "./App.css";
 import { ThemeProvider } from "styled-components";
 import { redTheme, greenTheme, blueTheme } from "./components/themes";
 import Navbar from "./components/Navbar";
+import Button from "./components/Button";
 
 function App() {
   return (
     <ThemeProvider theme={redTheme}>
       <main>
-        <Navbar />
+        <Navbar>
+          <Button>toggle theme</Button>
+        </Navbar>
       </main>
     </ThemeProvider>
   );
