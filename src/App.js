698e492990ee9703293f7b7ce7d4f2b89a563354
Jose Manuel <jmdcarq89@gmail.com>
2021-04-05 11:44:58 +0200

    Hero styled

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import "./App.css";
 import Hero from "./components/Hero";
 import image1 from "./images/image1.jpg";
 import image2 from "./images/image2.jpg";
-import image3 from "./images/image2.jpg";
 
 function App() {
   return (
     <div>
-      <Hero />
+      <Hero img={image1} />
+      <Hero img={image2} />
+      <Hero big />
     </div>
   );
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,12 @@
 import styled from "styled-components";
+import image3 from "../images/image3.jpg";
 
 const Hero = styled.div`
-  min-height: 100vh;
-  background-image: url(${(props) => props.img});
+  min-height: ${(props) => (props.big ? "100vh" : "50vh")};
+  background-image: url(${(props) => (props.img ? props.img : image3)});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 `;
 
 export default Hero;
