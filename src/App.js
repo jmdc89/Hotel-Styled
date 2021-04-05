6336b785d793908c0a1fa69b3d6e55e2fbb47eb5
Jose Manuel <jmdcarq89@gmail.com>
2021-04-05 19:23:54 +0200

    Animated Box

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import { RedBox, GreenBox } from "./components/Boxes";
+import { Box } from "./components/AnimatedBox";
 
 function App() {
   return (
     <div>
       <RedBox color="red" />
       <GreenBox color="green" />
+      <Box />
     </div>
   );
 }
