4fc08b30b04110bdb152b5754904eeade98ec913
Jose Manuel <jmdcarq89@gmail.com>
2021-04-06 12:56:22 +0200

    Hero comp

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Hero from "../globals/Hero";
+import aboutImg from "../../images/aboutBcg.jpeg";
 
 const Header = () => {
-  return <div>Hello From Header</div>;
+  return (
+    <>
+      <Hero img={aboutImg} />
+    </>
+  );
 };
 
 export default Header;
