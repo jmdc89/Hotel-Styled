b0b6b6f5fd6ee18b57e3651d6d9519c840d230cd
Jose Manuel <jmdcarq89@gmail.com>
2021-04-06 11:47:51 +0200

    globals

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Home from "./pages/Home";
+import GlobalStyles from "./components/globals/GlobalStyles";
 
-function App() {
+const App = () => {
   return (
     <>
+      <GlobalStyles />
       <Home />
     </>
   );
-}
+};
 
 export default App;
